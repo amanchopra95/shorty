61cf7f704a1669e31f06d125ec84df728c2e77c9
Aman Chopra <aman.chopra@taazaa.com>
2025-01-13 02:16:02 -0500

    atlas init

---
+++ atlas/src/ping/ping.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('ping')
+export class PingController {
+
+
+
+    @Get()
+    ping(): string {
+        return "Pinging..."
+    }
+}
